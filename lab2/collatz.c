2538983312d74bf821f7a9ea532b574a9bff822a
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-12 16:37:31 +0100

    start lab2

---
+++ lab2/collatz.c
@@ -0,0 +1,14 @@
+int collatz_conjecture(int input) {
+    if(input / 2)
+        return input / 2;
+    else
+        return 3 * input + 1;
+}
+
+int test_collatz_convergence(int input, int max_iter) {
+    int i = 0;
+    while ( i<max_iter && input != 1 ) {
+        input = collatz_conjecture(input);
+    }
+    return i;
+}
