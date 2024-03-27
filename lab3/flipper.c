ef46ab97fec229b3cfed36fe2de85b398627e1b1
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-27 23:30:19 +0100

    fix output directory, add run script

--- lab3/flipper.c
+++ lab3/flipper.c
@@ -8,20 +8,29 @@ int main() {
     DIR *dir;
     struct dirent *entry;
 
-    char input_dir_name[50] = "art/";
-    char output_dir_name[50] = "reversed/";
-    dir = opendir("art");
+    char input_dir_name[] = "art/";
+    char output_dir_name[] = "reversed/";
+
+    dir = opendir(input_dir_name);
     if (!dir) {
         printf("Can't read directory");
         return 1;
     }
 
     while ((entry = readdir(dir)) != NULL) {
+        if(entry->d_type != DT_REG) continue;
+
         char* input_file_name = entry->d_name;
-        char input_file_path[] = "art/";
-        strcat(input_file_path, input_file_name);
         if (strstr(input_file_name, ".txt") != NULL ) {
-            if (!reverse(input_file_path, input_file_name))
+            char input_file_path[100];
+            strcpy(input_file_path, input_dir_name);
+            strcat(input_file_path, input_file_name);
+
+            char output_file_path[100];
+            strcpy(output_file_path, output_dir_name);
+            strcat(output_file_path, input_file_name);
+
+            if (!reverse(input_file_path, output_file_path))
                 printf("Success!\n");
             else
                 printf("Something went wrong :l\n");
